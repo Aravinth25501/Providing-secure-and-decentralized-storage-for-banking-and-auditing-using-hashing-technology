37c044c0daf2b7215fdab6f66b181eef4a0ccd7e
Aravinth25501 <68952311+Aravinth25501@users.noreply.github.com>
2022-06-20 18:38:15 +0530

    User

--- models.py
+++ models.py
@@ -1,47 +1,47 @@
 from django.db import models
-from django.utils import timezone
+
 # Create your models here.
-
-class bank(models.Model):
-    name = models.CharField(max_length=50, null=True, unique=True)
-    managerid = models.IntegerField()
-    email = models.EmailField(null=True, unique=True)
+class user(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    area = models.CharField(max_length=50, null=True)
+    email = models.EmailField(null=True)
+    userid = models.CharField(max_length=50, null=True)
     password = models.CharField(max_length=50, null=True)
+    status = models.BooleanField(default=0)
 
-class scheme(models.Model):
-    name= models.CharField(max_length=50, null=True)
-    definition =models.TextField()
-    documentneeded = models.CharField(max_length=50, null=True)
-
-class loan(models.Model):
-    type = models.CharField(max_length=50, null=True)
-    definition = models.TextField(null=True)
-    needfilename = models.CharField(max_length=50, null=True)
-
-class schemeupload(models.Model):
-    schemename= models.CharField(max_length=50, null=True)
-    name = models.CharField(max_length=50, null=True)
-    email = models.EmailField()
-    dob = models.DateField()
-    contact = models.CharField(max_length=50, null=True)
-    patta = models.FileField()
-    chitta = models.FileField()
-    pattakey = models.CharField(max_length=50, null=True)
-    chittakey = models.CharField(max_length=50, null=True)
+class bankaccount(models.Model):
     userid = models.CharField(max_length=50, null=True)
-    uploadid = models.CharField(max_length=50, null=True)
-    access=models.BooleanField(null=True)
-
-class loanupload(models.Model):
-    loantype = models.CharField(max_length=50, null=True)
     name = models.CharField(max_length=50, null=True)
+    fathername= models.CharField(max_length=50, null=True)
     email = models.EmailField()
-    dob = models.DateField()
-    contact = models.CharField(max_length=50, null=True)
-    document = models.FileField()
-    key = models.CharField(max_length=50, null=True)
-    userid = models.CharField(max_length=50, null=True)
+    contact = models.BigIntegerField(null=True)
+    address = models.TextField()
+    aadhaar=models.FileField()
+    pancard=models.FileField()
+    photo=models.FileField()
+    aadhaarkey=models.CharField(max_length=50, null=True)
+    pancardkey=models.CharField(max_length=50, null=True)
+    photokey=models.CharField(max_length=50, null=True)
     uploadid = models.CharField(max_length=50, null=True)
     access = models.BooleanField(null=True)
 
+class node1(models.Model):
+    key= models.CharField(max_length=50, null=True)
+    hashvalue=models.TextField()
+    filename=models.CharField(max_length=50, null=True)
+
+class node2(models.Model):
+    key= models.CharField(max_length=50, null=True)
+    hashvalue=models.TextField()
+    filename=models.CharField(max_length=50, null=True)
+
+class node3(models.Model):
+    key= models.CharField(max_length=50, null=True)
+    hashvalue=models.TextField()
+    filename=models.CharField(max_length=50, null=True)
+
+class node4(models.Model):
+    key= models.CharField(max_length=50, null=True)
+    hashvalue=models.TextField()
+    filename=models.CharField(max_length=50, null=True)
 
--- views.py
+++ views.py
@@ -1,77 +1,297 @@
-from django.shortcuts import render,redirect,HttpResponse,Http404
-from .models import *
+import string
 from django.contrib import messages
-from admins.models import *
-from user.models import *
+from django.shortcuts import render,redirect
+from .models import *
 from django.conf import settings
-import PyPDF2,hmac
+from admins.models import *
+from audit.models import *
+from bank.models import *
+import random,hmac
+import PyPDF2
 from hashlib import *
-import os
 
 
 # Create your views here.
-def banks(request):
-    return render(request,'bank/bank.html')
+def home(request):
+    return render(request,'user/home.html')
+
+def bankuser(request):
+    return render(request, 'user/bank/bank.html')
+
+def audituser(request):
+    return render(request, 'user/audit/audit.html')
 
 def login(request,login):
     if request.method =='POST':
         if login == 'login':
-            email=request.POST['email']
-            password=request.POST['password']
             try:
-                detail=bank.objects.get(email=email,password=password)
-                request.session['bank']=detail.name
-                messages.success(request, "SUCCESSFULLY LOGGED IN AS BANK MANAGER")
-                return redirect('/bank')
+                email = request.POST['email']
+                password = request.POST['password']
+                area = request.POST['area']
+                detail=user.objects.get(email=email,password=password,area=area)
+                if area == 'BANK':
+                    request.session['user']=detail.userid
+                    messages.success(request, "SUCCESSFULLY LOGGED IN AS BANK USER")
+                    return redirect('/bankuser')
+                else:
+                    request.session['user'] = detail.userid
+                    messages.success(request, "SUCCESSFULLY LOGGED IN AS AUDIT USER")
+                    return redirect('/audituser')
             except:
                 messages.success(request, "INVALID EMAIL ID OR PASSWORD")
-                return redirect('/bank/login/login')
+                return redirect('/login/login')
         else:
             name= request.POST['name']
             email = request.POST['email']
             password = request.POST['password']
-            managerid = request.POST['id']
-            bankaccess(name=name,email=email,password=password,managerid=managerid).save()
-            messages.success(request, "SUCCESSFULLY REGISTERED AS BANK MANAGER")
+            area = request.POST['area']
+            userid=''
+            for i in range(5):
+                userid+=str(random.randint(0,9))
+            useraccess(name=name,email=email,password=password,area=area,userid=userid).save()
+            messages.success(request, "DETAIL HAS BEEN REGISTERED SUCCESSFULLY")
             return redirect('/')
-    return render(request,'bank/login.html',{'login':login})
+    return render(request,'user/login.html',{'login':login})
+
+def auditfiletable(request):
+    users=user.objects.get(userid=request.session['user'])
+    lists=requiredlist.objects.filter(userid=request.session['user']).values('requestid').distinct()
+    return render(request,'user/audit/auditfiletable.html',{'list':lists,'u':users})
+
+def auditfile(request,requestid):
+    lists=requiredlist.objects.filter(requestid=requestid)
+    return render(request, 'user/audit/auditfile.html', {'list': lists,'requestid':requestid})
+
+def typeview(request):
+    req = requirements.objects.all().order_by('type')
+    return render(request,'user/audit/typeview.html', {'list': req})
+
+def upload(request,requestid):
+    if request.method== 'POST':
+        lists = requiredlist.objects.filter(userid=request.session['user'],requestid=requestid)
+        for i in lists:
+            file=request.FILES[f'{i.list}']
+            ac = requiredlist.objects.get(userid=request.session['user'], list=i.list)
+            ac.file = file
+            ac.save()
+            #name=request.FILES[f'{i.list}'].name
+            print(ac.file)
+            files = open(f'{settings.MEDIA_ROOT}/{ac.file}', 'rb')
+            pdfReader = PyPDF2.PdfFileReader(files)
+            r = pdfReader.numPages
+            data = ''
+            for p in range(r):
+                data += pdfReader.getPage(p).extractText()
+            length = len(data) // 4
+            f1 = 0
+            f2 = length
+            initial = ''
+            for j in range(4):
+                data1 = data[f1:f2]
+                f3 = length
+                ran = ''
+                for k in range(5):
+                    ran += str(random.randint(0, 9))
+                if j==0:
+                    ac.key = ran
+                    ac.save()
+                    d = hmac.new(ran.encode(), data1.encode(), sha512).hexdigest()
+                    node1(key=ran,hashvalue=d,filename=i.list).save()
+                    initial=d
+                elif j==1:
+                    d = hmac.new(initial.encode(), data1.encode(), sha512).hexdigest()
+                    node2(key=initial, hashvalue=d, filename=i.list).save()
+                    initial = d
+                elif j==2:
+                    d = hmac.new(initial.encode(), data1.encode(), sha512).hexdigest()
+                    node3(key=initial, hashvalue=d, filename=i.list).save()
+                    initial = d
+                elif j==3:
+                    d = hmac.new(initial.encode(), data1.encode(), sha512).hexdigest()
+                    node4(key=initial, hashvalue=d, filename=i.list).save()
+                    initial = d
+                f1 += f3
+                f2 += f3
+    messages.success(request, "DOCUMENTS ARE UPLOADED SUCCESSFULLY")
+    return redirect('/auditfile')
+
+def create(request):
+    if request.method=='POST':
+        userid = request.session['user']
+        name=request.POST['name']
+        fname = request.POST['fname']
+        street= request.POST['street']
+        place= request.POST['place']
+        country=request.POST['country']
+        code = request.POST['code']
+        phone= request.POST['phone']
+        email= request.POST['email']
+        address=f'{street}, {place}, {country}, {code}'
+        aadhaar=request.FILES['aadhaar']
+        pan = request.FILES['pan']
+        photo = request.FILES['photo']
+        ada = ''.join(random.sample(string.digits, k=5))
+        bankaccount(userid=request.session['user'],name=name,fathername=fname,email=email,uploadid=ada,contact=phone,address=address,aadhaar=aadhaar,pancard=pan,photo=photo).save()
+        ac=bankaccount.objects.get(uploadid=ada)
+        li=[ac.aadhaar,ac.pancard,ac.photo]
+        list2=[f'{userid} AADHAAR',f'{userid} PAN',f'{userid} PHOTO',]
+        for i in range(3):
+            files = open(f'{settings.MEDIA_ROOT}/{li[i]}', 'rb')
+            pdfReader = PyPDF2.PdfFileReader(files)
+            r = pdfReader.numPages
+            data = ''
+            for p in range(r):
+                data += pdfReader.getPage(p).extractText()
+            length = len(data) // 4
+            f1 = 0
+            f2 = length
+            initial = ''
+            for j in range(4):
+                data1 = data[f1:f2]
+                f3 = length
+                ran = ''
+                for k in range(5):
+                    ran += str(random.randint(0, 9))
+                if j == 0:
+                    if i==0:
+                        ac.aadhaarkey=ran
+                    elif i==1:
+                        ac.pancardkey = ran
+                    else:
+                        ac.photokey=ran
+                    ac.save()
+                    d = hmac.new(ran.encode(), data1.encode(), sha512).hexdigest()
+                    node1(key=ran, hashvalue=d, filename=list2[i]).save()
+                    initial = d
+                elif j == 1:
+                    d = hmac.new(initial.encode(), data1.encode(), sha512).hexdigest()
+                    node2(key=initial, hashvalue=d, filename=list2[i]).save()
+                    initial = d
+                elif j == 2:
+                    d = hmac.new(initial.encode(), data1.encode(), sha512).hexdigest()
+                    node3(key=initial, hashvalue=d, filename=list2[i]).save()
+                    initial = d
+                elif j == 3:
+                    d = hmac.new(initial.encode(), data1.encode(), sha512).hexdigest()
+                    node4(key=initial, hashvalue=d, filename=list2[i]).save()
+                    initial = d
+                f1 += f3
+                f2 += f3
+        messages.success(request, "DOCUMENTS ARE UPLOADED SUCCESSFULLY")
+    return render(request,'user/bank/create.html')
 
 def status(request):
-    data=user.objects.filter(area='BANK')
-    return render(request,'bank/status.html',{'data':data})
+    stat=user.objects.get(userid=request.session['user'])
+    return render(request,'user/bank/status.html',{'status':stat})
 
-def account(request):
-    register=bankaccount.objects.all()
-    return render(request,'bank/account.html',{'register':register})
+def schemes(request):
+    l=scheme.objects.all()
+    k = schemeupload.objects.all()
+    list = []
+    dic = {'name': [], 'definition': [], 'documentneeded': [], 'status': []}
+    for i in range(len(l)):
+        dic = {}
+        dic['name'] = l[i].name
+        dic['definition'] = l[i].definition
+        dic['documentneeded'] = l[i].documentneeded
+        dic['status'] = 0
+        for j in k:
+            print(l[i].name == j.schemename)
+            print(j.userid,str(request.session['user']))
+            if l[i].name == j.schemename and j.userid == str(request.session['user']):
+                dic['status'] = 1
+        list.append(dic)
+    return render(request,'user/bank/scheme.html',{'scheme':list})
 
-def activate(request,userid):
-    user.objects.filter(userid=userid).update(status=True)
-    return redirect('/bank/account/')
+def uploadscheme(request,scheme):
+    if request.method == 'POST':
+        schemename =scheme
+        userid=request.session['user']
+        name = request.POST['name']
+        email = request.POST['email']
+        dob = request.POST['dob']
+        number = request.POST['number']
+        patta = request.FILES['patta']
+        chitta = request.FILES['chitta']
+        ada = ''.join(random.sample(string.digits, k=5))
+        schemeupload(schemename=schemename,name=name,email=email,dob=dob,contact=number,uploadid=ada,patta=patta,chitta=chitta,userid=request.session['user']).save()
+        ac = schemeupload.objects.get(uploadid=ada)
+        li = [ac.patta, ac.chitta]
+        list2 = [f'{userid} PATTA', f'{userid} CHITTA' ]
+        for i in range(2):
+            files = open(f'{settings.MEDIA_ROOT}/{li[i]}', 'rb')
+            pdfReader = PyPDF2.PdfFileReader(files)
+            r = pdfReader.numPages
+            data = ''
+            for p in range(r):
+                data += pdfReader.getPage(p).extractText()
+            length = len(data) // 4
+            f1 = 0
+            f2 = length
+            initial = ''
+            for j in range(4):
+                data1 = data[f1:f2]
+                f3 = length
+                ran = ''
+                for k in range(5):
+                    ran += str(random.randint(0, 9))
+                if j == 0:
+                    if i==0:
+                        ac.pattakey=ran
+                    else:
+                        ac.chittakey=ran
+                    ac.save()
+                    d = hmac.new(ran.encode(), data1.encode(), sha512).hexdigest()
+                    node1(key=ran, hashvalue=d, filename=list2[i]).save()
+                    initial = d
+                elif j == 1:
+                    d = hmac.new(initial.encode(), data1.encode(), sha512).hexdigest()
+                    node2(key=initial, hashvalue=d, filename=list2[i]).save()
+                    initial = d
+                elif j == 2:
+                    d = hmac.new(initial.encode(), data1.encode(), sha512).hexdigest()
+                    node3(key=initial, hashvalue=d, filename=list2[i]).save()
+                    initial = d
+                elif j == 3:
+                    d = hmac.new(initial.encode(), data1.encode(), sha512).hexdigest()
+                    node4(key=initial, hashvalue=d, filename=list2[i]).save()
+                    initial = d
+                f1 += f3
+                f2 += f3
+        messages.success(request, "DOCUMENTS ARE UPLOADED SUCCESSFULLY")
+    return render(request,'user/bank/schemeupload.html',{'scheme':scheme})
 
-def registerdocument(request,userid):
-    data = bankaccount.objects.get(userid=userid)
-    return render(request,'bank/document1.html',{'data':data})
+def loans(request):
+    l=loan.objects.all()
+    k=loanupload.objects.all()
+    list=[]
+    dic={'type':[],'definition':[],'needfilename':[],'status':[]}
+    for i in range(len(l)):
+        dic={}
+        dic['type']=l[i].type
+        dic['definition']=l[i].definition
+        dic['needfilename']=l[i].needfilename
+        dic['status']=0
+        for j in k:
+            if l[i].needfilename == j.loantype and j.userid==str(request.session['user']):
+                dic['status']=1
+        list.append(dic)
+    return render(request,'user/bank/loan.html',{'loan':l,'dic':list})
 
-def appliedschemes(request,schemename):
-    data = schemeupload.objects.filter(schemename=schemename)
-    return render(request,'bank/appliedscheme.html',{'data':data})
-
-def documentview2(request,uploadid):
-    data = schemeupload.objects.get(uploadid=uploadid)
-    return render(request,'bank/document1.html',{'data2':data})
-
-def appliedloan(request,loan):
-    loandata = loanupload.objects.filter(loantype=loan)
-    return render(request,'bank/appliedloan.html',{'loandata':loandata})
-
-def documentview3(request,uploadid):
-    data = loanupload.objects.get(uploadid=uploadid)
-    return render(request,'bank/document1.html',{'data3':data})
-
-def download1(request,file):
-    if request.method== 'POST':
-        files = open(f'{settings.MEDIA_ROOT}/{file}', 'rb')
-        key = request.POST['key']
+def uploadloan(request,loan):
+    if request.method == 'POST':
+        userid = request.session['user']
+        name = request.POST['name']
+        email = request.POST['email']
+        dob = request.POST['dob']
+        number = request.POST['number']
+        document = request.FILES['doc']
+        ada = ''.join(random.sample(string.digits, k=5))
+        loanupload(loantype=loan,name=name,email=email,dob=dob,contact=number,uploadid=ada,document=document,userid=request.session['user']).save()
+        ac = loanupload.objects.get(uploadid=ada)
+        li = ac.document
+        list2 = f'{userid} {ac.loantype}'
+        files = open(f'{settings.MEDIA_ROOT}/{li}', 'rb')
         pdfReader = PyPDF2.PdfFileReader(files)
         r = pdfReader.numPages
         data = ''
@@ -81,96 +301,36 @@ def download1(request,file):
         f1 = 0
         f2 = length
         initial = ''
-        valid = 0
         for j in range(4):
             data1 = data[f1:f2]
             f3 = length
+            ran = ''
+            for k in range(5):
+                ran += str(random.randint(0, 9))
             if j == 0:
-                d = hmac.new(key.encode(), data1.encode(), sha512).hexdigest()
-                times = node1.objects.all()
-                for i in times:
-                    if d == i.hashvalue:
-                        valid += 1
+                ac.key = ran
+                ac.save()
+                d = hmac.new(ran.encode(), data1.encode(), sha512).hexdigest()
+                node1(key=ran, hashvalue=d, filename=list2).save()
                 initial = d
             elif j == 1:
                 d = hmac.new(initial.encode(), data1.encode(), sha512).hexdigest()
-                times = node2.objects.all()
-                for i in times:
-                    if d == i.hashvalue:
-                        valid += 1
+                node2(key=initial, hashvalue=d, filename=list2).save()
                 initial = d
             elif j == 2:
                 d = hmac.new(initial.encode(), data1.encode(), sha512).hexdigest()
-                times = node3.objects.all()
-                for i in times:
-                    if d == i.hashvalue:
-                        valid += 1
+                node3(key=initial, hashvalue=d, filename=list2).save()
                 initial = d
             elif j == 3:
                 d = hmac.new(initial.encode(), data1.encode(), sha512).hexdigest()
-                times = node4.objects.all()
-                for i in times:
-                    if d == i.hashvalue:
-                        valid += 1
+                node4(key=initial, hashvalue=d, filename=list2).save()
                 initial = d
             f1 += f3
             f2 += f3
-        if valid == 4:
-            return redirect(f'/audit/alpha/{file}')
-        else:
-            return HttpResponse("YOU HAVE ENTERED INVALID KEY")
-    return redirect('/bank/account/')
-
-def downloads(request,path):
-    file_path = os.path.join(settings.MEDIA_ROOT, str(path))
-    if os.path.exists(file_path):
-        with open(file_path, 'rb') as fh:
-            response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
-            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
-            return response
-    raise Http404
-
-def postscheme(request):
-    if request.method =='POST':
-        name=request.POST['name']
-        docname = request.POST['docname']
-        schemes = request.POST['message']
-        scheme(name=name,definition=schemes,documentneeded=docname).save()
-        messages.success(request, "SCHEME HAS BEEN UPLOADED")
-    return render(request,'bank/scheme.html')
-
-def postloan(request):
-    if request.method =='POST':
-        name=request.POST['loan']
-        need = request.POST['need']
-        description = request.POST['description']
-        loan(type=name,definition=description,needfilename=need).save()
-        messages.success(request, "LOAN HAS BEEN UPLOADED")
-    return render(request,'bank/loanupload.html')
-
-def schemelist(request):
-    schemes=scheme.objects.all()
-    return render(request,'bank/schemelist.html',{'scheme':schemes})
-
-def loans(request):
-    loans=loan.objects.all()
-    return render(request,'bank/loan.html',{'loan':loans})
-
-def keyaccess(request,uploadid,type):
-    if type=="scheme":
-        schemeupload.objects.filter(uploadid=uploadid).update(access=0)
-        messages.success(request, "KEY FOR DOCUMENT UNDER SCHEME HAS BEEN REQUESTED")
-        return redirect(f'/bank/documentview2/{uploadid}')
-    elif type=="loan":
-        messages.success(request, "KEY FOR DOCUMENT UNDER LOAN HAS BEEN REQUESTED")
-        loanupload.objects.filter(uploadid=uploadid).update(access=0)
-        return redirect(f'/bank/documentview3/{uploadid}')
-    else:
-        messages.success(request, "KEY FOR DOCUMENT FOR THE CREATION OF ACCOUNT HAS BEEN REQUESTED")
-        bankaccount.objects.filter(uploadid=uploadid).update(access=0)
-        return redirect(f'/bank/account/')
+        messages.success(request, "DOCUMENTS ARE UPLOADED SUCCESSFULLY")
+    return render(request,'user/bank/loanupload.html',{'loan':loan})
 
 def logout(request):
-    del request.session['bank']
-    messages.success(request, "LOGGED OUT SUCCESSFULLY")
+    del request.session['user']
+    messages.success(request, "SUCCESSFULLY LOGGED OUT")
     return redirect('/')
